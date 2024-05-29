6648d6afe55c3b2745fc34b2c6f80becc6b6a97d
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-05-29 13:53:16 +0300

    visualize_object()
    -created func to visualize each object

--- part_one.py
+++ part_one.py
@@ -9,3 +9,11 @@ swallow = np.array(
     [[-5, 4], [-7, 4], [-9, 6], [-11, 6], [-12, 5], [-14, 5], [-12, 4], [-14, 3], [-12, 3], [-11, 2], [-10, 2],
      [-9, 1], [-9, 0], [-8, -2], [0, -3], [3, -2], [19, -2], [4, 0], [19, 4], [4, 2], [2, 3], [6, 9], [10, 11],
      [3, 11], [1, 10], [-5, 4]])
+
+
+def visualize_object(object_points):
+    x_list = [coordinates[0] for coordinates in object_points]
+    y_list = [coordinates[1] for coordinates in object_points]
+    plt.plot(x_list, y_list)
+    plt.show()
+
