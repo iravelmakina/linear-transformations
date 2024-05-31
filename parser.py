3ced19c5fa325aa0c5aa597738b7f34de4fb4fc2
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-05-31 20:07:33 +0300

    Revert "oop_refactoring"
    This reverts commit 76573328c813d1a03fc53adfe812aa29620ff2d6.

--- parser.py
+++ parser.py
@@ -1,96 +1,61 @@
 from part_one import *
 
 
-class CommandParser:
-    def __init__(self, object_dict):
-        self.object_dict = object_dict
-        self.current_object = None
-        self.transformer = None
-
-    @staticmethod
-    def print_menu():
-        print("1. Rotate object\n"
-              "2. Scale object\n"
-              "3. Reflect relative to certain axis\n"
-              "4. Shear the object\n"
-              "5. Transform with custom matrix\n"
-              "6. Change current object\n")
-
-    @staticmethod
-    def is_valid_input(user_choice):
-        return (user_choice.isdigit() and 0 < int(user_choice) < 7) or user_choice in ["m", "e"]
-
-    def ask_and_validate_object(self):
-        object_choice = input(f"Please, choose the object (available options: {', '.join(self.object_dict.keys())}): ").lower()
-        while object_choice not in self.object_dict:
-            print("Invalid input. Please, try again: ")
-            object_choice = input(f"Please, choose the object (available options: {', '.join(self.object_dict.keys())}): ").lower()
-        self.current_object = self.object_dict[object_choice]
-        self.transformer = Transformer()
-        print(f"Current object set to {self.current_object.name}")
-
-    def process_command(self, user_choice):
-        if not self.current_object and user_choice != "6":
-            self.ask_and_validate_object()
-
-        match user_choice:
-            case "1":
-                print("Let's rotate the object!")
-                self.transformer.rotate(self.current_object)
-            case "2":
-                print("Let's scale the object!")
-                self.transformer.scale(self.current_object)
-            case "3":
-                print("Let's reflect the object!")
-                self.transformer.reflect(self.current_object)
-            case "4":
-                print("Let's shear the object!")
-                self.transformer.shear(self.current_object)
-            case "5":
-                print("Let's transform the object with custom matrix!")
-                self.transformer.transform(self.current_object)
-            case "6":
-                print("Let's change the current object!")
-                self.ask_and_validate_object()
-            case _:
-                print("Unable to process the command. Please, try again.")
-                return
+def process_command(user_choice):
+    match user_choice:
+        case "1":
+            print("Rotating the object...")
+            rotate_object(hare)
+            rotate_object(swallow)
+        case "2":
+            print("Scaling the object...")
+            scale_object(hare)
+            scale_object(swallow)
+        case "3":
+            print("Reflecting the object...")
+            reflect_object(hare)
+            reflect_object(swallow)
+        case "4":
+            print("Shearing the object...")
+            shear_object(hare)
+            shear_object(swallow)
+        case "5":
+            print("Transforming with custom matrix...")
+            transform_object(hare)
+            transform_object(swallow)
+        case _:
+            print("Unable to process the command. Please, try again.")
+            return
 
 
-object_dict = {"hare": hare, "swallow": swallow, "bird": shape}
+def print_menu():
+    print("1. Rotate object\n"
+          "2. Scale object\n"
+          "3. Reflect relative to certain axis\n"
+          "4. Shear the object\n"
+          "5. Transform with custom matrix\n")
+
+
+def is_valid_input(user_choice):
+    return (user_choice.isdigit() and 0 < int(user_choice) < 6) or user_choice in ["m", "e"]
 
 
 def main():
     print("Welcome to object transformer! Enter 'm' to see all the commands and 'e' to exit")
-    command_parser = CommandParser(object_dict)
     user_choice = ""
     while user_choice != "e":
         user_choice = (input("Please, choose your command: ")).lower()
-        while not command_parser.is_valid_input(user_choice):
+        while not is_valid_input(user_choice):
             print("Invalid input. Please, try again: ")
             user_choice = (input("Please, choose your command: ")).lower()
 
         if user_choice == "m":
-            command_parser.print_menu()
+            print_menu()
         else:
-            command_parser.process_command(user_choice)
+            process_command(user_choice)
     print("Okay, bye!")
     return
 
 
 main()
 
-hare = np.array(
-    [[5, 1], [6, 2], [6, 3], [5, 6], [4, 7], [5, 8], [6, 8], [8, 9], [9, 9], [7, 8], [9, 8], [6, 7], [7, 6], [9, 6],
-     [11, 5], [12, 3], [12, 2], [13, 3], [12, 1], [7, 1], [8, 2], [9, 2], [8, 3], [6, 1], [5, 1]])
-hare = Object("hare", hare)
-
-swallow = np.array(
-    [[-5, 4], [-7, 4], [-9, 6], [-11, 6], [-12, 5], [-14, 5], [-12, 4], [-14, 3], [-12, 3], [-11, 2], [-10, 2],
-     [-9, 1], [-9, 0], [-8, -2], [0, -3], [3, -2], [19, -2], [4, 0], [19, 4], [4, 2], [2, 3], [6, 9], [10, 11],
-     [3, 11], [1, 10], [-5, 4]])
-swallow = Object("swallow", swallow)
-
-shape = np.array([[0, 0, 0], [1, 2, 1], [2, 3, 1], [3, 3, 0], [2, 2, -1], [1, 0, -1], [0, -1, 0],
-                  [-1, -2, 1], [-2, -3, 1], [-3, -3, 0], [-2, -2, -1], [-1, 0, -1], [0, 0, 0]])
-shape = Object("bird", shape)
--- part_one.py
+++ part_one.py
@@ -1,156 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+hare = np.array(
+    [[5, 1], [6, 2], [6, 3], [5, 6], [4, 7], [5, 8], [6, 8], [8, 9], [9, 9], [7, 8], [9, 8], [6, 7], [7, 6], [9, 6],
+     [11, 5], [12, 3], [12, 2], [13, 3], [12, 1], [7, 1], [8, 2], [9, 2], [8, 3], [6, 1], [5, 1]])
 
-class Object:
-    def __init__(self, name, points):
-        self.name = name
-        self.points = points
-        self.dimension = points.shape[1]
+swallow = np.array(
+    [[-5, 4], [-7, 4], [-9, 6], [-11, 6], [-12, 5], [-14, 5], [-12, 4], [-14, 3], [-12, 3], [-11, 2], [-10, 2],
+     [-9, 1], [-9, 0], [-8, -2], [0, -3], [3, -2], [19, -2], [4, 0], [19, 4], [4, 2], [2, 3], [6, 9], [10, 11],
+     [3, 11], [1, 10], [-5, 4]])
 
-    def __str__(self):
-        return f"{self.dimension}D object '{self.name}' with points:\n{self.points}"
+shape = np.array([[0, 0, 0], [1, 2, 1], [2, 3, 1], [3, 3, 0], [2, 2, -1], [1, 0, -1], [0, -1, 0],
+                  [-1, -2, 1], [-2, -3, 1], [-3, -3, 0], [-2, -2, -1], [-1, 0, -1], [0, 0, 0]])
 
-    def visualize(self):
-        if self.dimension == 2:
-            self._visualize_2d()
-        elif self.dimension == 3:
-            self._visualize_3d()
-        else:
-            print("Visualization for such an object is not supported.")
-
-    def _visualize_2d(self):
-        x_list = [coordinates[0] for coordinates in self.points]
-        y_list = [coordinates[1] for coordinates in self.points]
-        plt.plot(x_list, y_list)
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        plt.title(f'2D Object: {self.name}')
-        plt.show()
-
-    def _visualize_3d(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        x_list = [coordinates[0] for coordinates in self.points]
-        y_list = [coordinates[1] for coordinates in self.points]
-        z_list = [coordinates[2] for coordinates in self.points]
-        ax.plot(x_list, y_list, z_list)
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        plt.title(f'3D Object: {self.name}')
-        plt.show()
+object_dict = {"hare": hare, "swallow": swallow, "bird": shape}
 
 
-class Transformer:
-    @staticmethod
-    def rotate(object):
-        angle_in_degrees = ask_and_validate_coefficient("angle")
-        angle_in_radians = angle_in_degrees * np.pi / 180
-
-        if object.dimension == 2:
-            rotation_matrix = np.array([[np.cos(angle_in_radians), -np.sin(angle_in_radians)],
-                                        [np.sin(angle_in_radians), np.cos(angle_in_radians)]])
-        elif object.dimension == 3:
-            axis = ask_and_validate_axis("rotate", object_3d=True)
-            if axis == "x":
-                rotation_matrix = np.array([[1, 0, 0],
-                                            [0, np.cos(angle_in_radians), -np.sin(angle_in_radians)],
-                                            [0, np.sin(angle_in_radians), np.cos(angle_in_radians)]])
-            elif axis == "y":
-                rotation_matrix = np.array([[np.cos(angle_in_radians), 0, -np.sin(angle_in_radians)],
-                                            [0, 1, 0],
-                                            [np.sin(angle_in_radians), 0, np.cos(angle_in_radians)]])
-            else:
-                rotation_matrix = np.array([[np.cos(angle_in_radians), -np.sin(angle_in_radians), 0],
-                                            [np.sin(angle_in_radians), np.cos(angle_in_radians), 0],
-                                            [0, 0, 1]])
-        else:
-            print("Rotation for such an object is not supported.")
-            return
-
-        object.points = np.dot(object.points, rotation_matrix.T)
-        print_matrix(object.points, "Rotated")
-        object.visualize()
-
-    @staticmethod
-    def scale(object):
-        scale_factor = ask_and_validate_coefficient("scale factor")
-        scaled_object = scale_factor * object.points
-        print_matrix(scaled_object, "Scaled")
-        object.points = scaled_object
-        object.visualize()
-
-    @staticmethod
-    def reflect(object):
-        if object.dimension == 2:
-            axis = ask_and_validate_axis("reflect")
-            if axis == "x":
-                transformation_matrix = np.array([[1, 0], [0, -1]])
-            else:
-                transformation_matrix = np.array([[-1, 0], [0, 1]])
-        elif object.dimension == 3:
-            axis = ask_and_validate_axis("reflect", object_3d=True)
-            if axis == "x":
-                transformation_matrix = np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
-            elif axis == "y":
-                transformation_matrix = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]])
-            else:
-                transformation_matrix = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]])
-        else:
-            print("Reflection for such an object is not supported.")
-            return
-        reflected_object = np.dot(object.points, transformation_matrix.T)
-        print_matrix(reflected_object, f"Reflected relative to {axis}-axis")
-        object.points = reflected_object
-        object.visualize()
-
-    @staticmethod
-    def shear(object):
-        shear_factor = ask_and_validate_coefficient("shear factor")
-        if object.dimension == 2:
-            axis = ask_and_validate_axis("shear")
-            if axis == "x":
-                shear_matrix = np.array([[1, shear_factor], [0, 1]])
-            else:
-                shear_matrix = np.array([[1, 0], [shear_factor, 1]])
-        elif object.dimension == 3:
-            axis = ask_and_validate_axis("shear", object_3d=True)
-            if axis == "x":
-                shear_matrix = np.array([[1, shear_factor, shear_factor], [0, 1, 0], [0, 0, 1]])
-            elif axis == "y":
-                shear_matrix = np.array([[1, 0, 0], [shear_factor, 1, shear_factor], [0, 0, 1]])
-            else:
-                shear_matrix = np.array([[1, 0, 0], [0, 1, 0], [shear_factor, shear_factor, 1]])
-        else:
-            print("Shearing for such an object is not supported.")
-            return
-        sheared_object = np.dot(object.points, shear_matrix.T)
-        print_matrix(sheared_object, f"Sheared relative to {axis}-axis")
-        object.points = sheared_object
-        object.visualize()
-
-    @staticmethod
-    def transform(object):
-        if object.dimension == 2:
-            custom_matrix = ask_and_validate_matrix("n * 2", 2)
-        elif object.dimension == 3:
-            custom_matrix = ask_and_validate_matrix("n * 3", 3)
-        else:
-            print("Transformation for such an object is not supported.")
-            return
-        transformed_object = np.dot(object.points, custom_matrix.T)
-        print_matrix(transformed_object, "Transformed")
-        object.points = transformed_object
-        object.visualize()
-
-
+# func to validate integer input
 def ask_and_validate_coefficient(prompt):
     value = input(f"Please, enter {prompt}: ").lower()
     while not is_float(value):
         print("Invalid input. Please, try again: ")
         value = input(f"Please, enter {prompt}: ").lower()
-    return float(value)
+    return float(value)  # OK
 
 
 def is_float(value):
@@ -158,7 +30,7 @@ def is_float(value):
         float(value)
         return True
     except ValueError:
-        return False
+        return False  # OK
 
 
 def ask_and_validate_axis(prompt, object_3d=False):
@@ -168,7 +40,7 @@ def ask_and_validate_axis(prompt, object_3d=False):
     while axis not in valid_axes:
         print("Invalid input. Please, try again: ")
         axis = input(f"Please, choose the axis you want to {prompt} the object relative to ({axes}): ").lower()
-    return axis
+    return axis  # OK
 
 
 def ask_and_validate_matrix(prompt, dimension):
@@ -176,7 +48,7 @@ def ask_and_validate_matrix(prompt, dimension):
     while not is_valid_matrix(matrix, dimension):
         print("Invalid input. Please, try again: ")
         matrix = input(f"Please, enter your custom {prompt} matrix: ")
-    return np.array([list(map(float, row.split())) for row in matrix.split(';')])
+    return np.array([list(map(float, row.split())) for row in matrix.split(';')])  # OK
 
 
 def is_valid_matrix(matrix, dimension):
@@ -187,34 +59,156 @@ def is_valid_matrix(matrix, dimension):
         for element in elements:
             if not is_float(element):
                 return False
-    return True
+    return True  # OK
 
 
+# func to choose object
 def ask_and_validate_object(object_dict, prompt):
     object_choice = input(f"Please, choose the object you want to {prompt}: ").lower()
-    while object_choice not in object_dict:
+    while object_choice not in ["hare", "swallow", "bird"]:
         print("Invalid input. Please, try again: ")
         object_choice = input(f"Please, choose the object you want to {prompt}: ").lower()
-    return object_dict[object_choice]
+    return object_dict[object_choice] # OK
+
+
+def is_2d_object(object_points):
+    return object_points.shape[1] == 2
+
+
+def is_3d_object(object_points):
+    return object_points.shape[1] == 3
 
 
 def print_matrix(matrix, transformation_adjective):
     print(f"{transformation_adjective} matrix: ")
     for row in matrix:
-        print(row)
+        print(row) # OK
 
 
-hare = np.array(
-    [[5, 1], [6, 2], [6, 3], [5, 6], [4, 7], [5, 8], [6, 8], [8, 9], [9, 9], [7, 8], [9, 8], [6, 7], [7, 6], [9, 6],
-     [11, 5], [12, 3], [12, 2], [13, 3], [12, 1], [7, 1], [8, 2], [9, 2], [8, 3], [6, 1], [5, 1]])
-hare = Object("hare", hare)
+def visualize_object(object_points):
+    if is_2d_object(object_points):
+        x_list = [coordinates[0] for coordinates in object_points]
+        y_list = [coordinates[1] for coordinates in object_points]
+        plt.plot(x_list, y_list)
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('2D Object')
+        plt.show()
+    elif is_3d_object(object_points):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        x_list = [coordinates[0] for coordinates in object_points]
+        y_list = [coordinates[1] for coordinates in object_points]
+        z_list = [coordinates[2] for coordinates in object_points]
+        plt.plot(x_list, y_list, z_list)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.title('3D Object')
+        plt.show()
+    else:
+        print("Visualization for such an object is not supported.")  # OK
 
-swallow = np.array(
-    [[-5, 4], [-7, 4], [-9, 6], [-11, 6], [-12, 5], [-14, 5], [-12, 4], [-14, 3], [-12, 3], [-11, 2], [-10, 2],
-     [-9, 1], [-9, 0], [-8, -2], [0, -3], [3, -2], [19, -2], [4, 0], [19, 4], [4, 2], [2, 3], [6, 9], [10, 11],
-     [3, 11], [1, 10], [-5, 4]])
-swallow = Object("swallow", swallow)
 
-shape = np.array([[0, 0, 0], [1, 2, 1], [2, 3, 1], [3, 3, 0], [2, 2, -1], [1, 0, -1], [0, -1, 0],
-                  [-1, -2, 1], [-2, -3, 1], [-3, -3, 0], [-2, -2, -1], [-1, 0, -1], [0, 0, 0]])
-shape = Object("bird", shape)
+def print_and_visualize(matrix, transformation_adjective):
+    print_matrix(matrix, transformation_adjective)
+    visualize_object(matrix) # OK
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def rotate_object():
+    object_points = ask_and_validate_object(object_dict, "rotate")
+    angle_in_degrees = ask_and_validate_coefficient("angle")
+    angle_in_radians = angle_in_degrees * np.pi / 180
+
+    if is_2d_object(object_points):
+        rotation_matrix = np.array([[np.cos(angle_in_radians), -np.sin(angle_in_radians)],
+                                    [np.sin(angle_in_radians), np.cos(angle_in_radians)]])
+        rotated_object = np.dot(rotation_matrix, object_points.T).T
+    elif is_3d_object(object_points):
+        axis = ask_and_validate_axis("rotate", object_3d=True)
+        if axis == "x":
+            rotation_matrix = np.array([[1, 0, 0],
+                                      [0, np.cos(angle_in_radians), -np.sin(angle_in_radians)],
+                                      [0, np.sin(angle_in_radians), np.cos(angle_in_radians)]])
+        elif axis == "y":
+            rotation_matrix = np.array([[np.cos(angle_in_radians), 0, -np.sin(angle_in_radians)],
+                                      [0, 1, 0],
+                                      [np.sin(angle_in_radians), 0, np.cos(angle_in_radians)]])
+        else:
+            rotation_matrix = np.array([[np.cos(angle_in_radians), -np.sin(angle_in_radians), 0],
+                                      [np.sin(angle_in_radians), np.cos(angle_in_radians), 0],
+                                      [0, 0, 1]])
+        rotated_object = np.dot(rotation_matrix, object_points.T).T
+    else:
+        print("Rotation for such an object is not supported.")
+        return
+    print_and_visualize(rotated_object, "Rotated")
+
+
+def scale_object():
+    object_points = ask_and_validate_object(object_dict, "scale")
+    scale_factor = ask_and_validate_coefficient("scale factor")
+    scaled_object = scale_factor * object_points
+    print_and_visualize(scaled_object, "Scaled") # OK
+
+
+def reflect_object():
+    object_points = ask_and_validate_object(object_dict, "reflect")
+    if is_2d_object(object_points):
+        axis = ask_and_validate_axis("reflect")
+        if axis == "x":
+            transformation_matrix = np.array([[1, 0], [0, -1]])
+        else:
+            transformation_matrix = np.array([[-1, 0], [0, 1]])
+    elif is_3d_object(object_points):
+        axis = ask_and_validate_axis("reflect", object_3d=True)
+        if axis == "x":
+            transformation_matrix = np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
+        elif axis == "y":
+            transformation_matrix = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]])
+        else:
+            transformation_matrix = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]])
+    else:
+        print("Reflection for such an object is not supported.")
+        return
+    reflected_object = np.dot(transformation_matrix, object_points.T).T
+    print_and_visualize(reflected_object, f"Reflected relative to {axis}-axis")
+
+
+def shear_object():
+    object_points = ask_and_validate_object(object_dict, "shear")
+    shear_factor = ask_and_validate_coefficient("shear factor")
+    if is_2d_object(object_points):
+        axis = ask_and_validate_axis("shear")
+        if axis == "x":
+            shear_matrix = np.array([[1, shear_factor], [0, 1]])
+        else:
+            shear_matrix = np.array([[1, 0], [shear_factor, 1]])
+    elif is_3d_object(object_points):
+        axis = ask_and_validate_axis("shear", object_3d=True)
+        if axis == "x":
+            shear_matrix = np.array([[1, shear_factor, shear_factor], [0, 1, 0], [0, 0, 1]])
+        elif axis == "y":
+            shear_matrix = np.array([[1, 0, 0], [shear_factor, 1, shear_factor], [0, 0, 1]])
+        else:
+            shear_matrix = np.array([[1, 0, 0], [0, 1, 0], [shear_factor, shear_factor, 1]])
+    else:
+        print("Shearing for such an object is not supported.")
+        return
+    sheared_object = np.dot(shear_matrix, object_points.T).T
+    print_and_visualize(sheared_object, f"Sheared relative to {axis}-axis")
+
+
+def transform_object():
+    object_points = ask_and_validate_object(object_dict, "transform")
+    if is_2d_object(object_points):
+        custom_matrix = ask_and_validate_matrix("n * 2", 2)
+    elif is_3d_object(object_points):
+        custom_matrix = ask_and_validate_matrix("n * 3", 3)
+    else:
+        print("Transformation for such an object is not supported.")
+        return
+    transformed_object = np.dot(custom_matrix, object_points.T).T
+    print_and_visualize(transformed_object, "Transformed")
