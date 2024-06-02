c778ab74e1385c89c1a786edaa07be7c4ce05428
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-06-02 18:44:30 +0300

    main.py
    -created main.py file with main func

--- parser.py
+++ parser.py
@@ -135,33 +135,3 @@ def switch_mode(current_mode):
         return new_mode, object_or_image
     else:
         return new_mode, None
-
-
-def main():
-    print("Welcome to object transformer! Enter 'm' to see all the commands and 'e' to exit")
-    mode = "manual"
-    object_or_image = None
-    image, h, w = initialize_image(image_path)
-
-    while True:
-        user_choice = (input("Please, choose your command: ")).lower()
-        while not is_valid_input(user_choice, mode, object_or_image):
-            print("Invalid input. Please, try again: ")
-            user_choice = (input("Please, choose your command: ")).lower()
-
-        if user_choice == "e":
-            break
-        elif user_choice == "m":
-            print_menu()
-        elif user_choice == "mode":
-            mode, object_or_image = switch_mode(mode)
-        elif user_choice == "change":
-            object_or_image = ask_and_validate_object_or_image()
-        else:
-            process_command(user_choice, mode, image, h, w, object_or_image)
-
-    print("Okay, bye!")
-    return
-
-
-main()
